7b3fffa3211bd2e2cf0a39ca329bb5982fff2b1d
shinkeionzu-cmyk <shinkeionzu@gmail.com>
2026-03-29 13:46:47 +0800

    Update license_engine.js

--- core/license_engine.js
+++ core/license_engine.js
@@ -1,1 +1,19 @@
-.gitkeep
+const crypto = require('crypto');
+
+/**
+ * KETICK LICENSE ENGINE
+ * Menjana kunci rahsia yang sukar digodam.
+ */
+const LicenseEngine = {
+    generateKey: (clientId) => {
+        const secret = "KETICK_EMPIRE_SECRET_2026"; // Jangan kongsi ini
+        return crypto.createHmac('sha256', secret)
+                     .update(clientId + Date.now())
+                     .digest('hex')
+                     .substring(0, 16)
+                     .toUpperCase();
+    }
+};
+
+module.exports = LicenseEngine;
+
